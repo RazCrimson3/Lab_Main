2b2874049ad5ad0e1a214174b9b22ec94c3ac79c
RazCrimson3 <bharathvigneshjo@gmail.com>
2019-07-12 12:05:18 +0530

    Lab_end

--- Programs_PC/ws3/ws3_05.c
+++ Programs_PC/ws3/ws3_05.c
@@ -4,21 +4,21 @@ int main()
 {
 	while(1)
 	{
-		short Input,Sum=0,Choice=0;
+		short Input,Sum=0;
 		printf("\n\t\t##### Integer Sum #####");
-		printf("\n\nEnter the Number(1-999)  : ");
+		printf("\n\nEnter the Number  : ");
 		scanf("%hi",&Input);
 		printf("\nAfter Operation :\nRemainder        Num\n---------        ---");
 		for(int i=1;Input>0;i++)
 		{
 			Sum+=Input%10;
-			printf("\n%hi\t\t %hi",Choice=(Input%10),Input/10);
+			printf("\n%hi\t\t %hi",(Input%10),Input/10);
 			Input/=10;
 		}
 		printf("\n\nThe Sum of digits is : %hi",Sum);
 		printf("\n\nMore Items? -1 to stop, other to continue : ");
-		scanf("%hi",&Choice);
-		if(Choice==-1)
+		scanf("%hi",&Sum);
+		if(Sum==-1)
 			break;
 
 	}
