53a1c45e2d786c8462d4f6544b18de8254fe0cba
19PW08@cs2k16.amcs.ws2k12.psgcs.edu <19PW08@cs2k16.amcs.ws2k12.psgcs.edu>
2019-07-24 19:05:11 -0700

    CPP to C

---
+++ Programs_PC/ws3/w3_15.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned short Input;
+    printf("\nEnter the Input : ");
+    scanf("%hu",&Input);
+    if(Input>=100&&Input<=999)
+    {
+        if(Input-((Input%10)+((int)(Input/100)*100)==((Input%10)+(Input/100))))
+            printf("\nThe Middle Digit is the sum of the other two digits");
+        else
+        {
+            printf("\nThe Middle Digit is not the sum of the other two digits");
+        }
+    }
+    else
+    {
+        printf("\nERROR!\nEnter a three digit number");
+        exit(7);
+    }
+    printf("\n");
+    return 0;
+}
