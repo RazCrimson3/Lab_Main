a36ba7d41e0cf7b6ed6124e151834fbba070576f
19PW08@cs2k16.amcs.ws2k12.psgcs.edu <19PW08@cs2k16.amcs.ws2k12.psgcs.edu>
2019-07-25 10:08:06 +0530

    ch

--- Programs_PC/ws3/ws3_07.c
+++ Programs_PC/ws3/ws3_07.c
@@ -1,64 +1,76 @@
 #include <stdio.h>
 
-int main()
+int print_digit(int Num)
 {
-	int Num;
-	printf("\nEnter the Input : ");
-	scanf("%ld", &Num);
-	if (Num == 0)
-		printf("\nZero.");
 	if (Num >= 100 || Num <= -1)
 	{
 		printf("\nERROR!!!");
 		return(-1);
 	}
-	else if (Num == 0)
+	else if(Num==0)
 		return 0;
 	if (Num >= 20 && Num < 100)
+	{	
 		switch (Num / 10)
 		{
-		case 2: printf("Twenty ");    break;
-		case 3: printf("Thirty ");    break;
-		case 4: printf("Fourty ");    break;
-		case 5: printf("Fifty ");     break;
-		case 6: printf("Sixty ");     break;
-		case 7: printf("Seventy ");   break;
-		case 8: printf("Eighty ");    break;
-		case 9: printf("Ninety ");    break;
+		case 1: printf("Ten ");		 break;
+		case 2: printf("Twenty ");	 break;
+		case 3: printf("Thirty ");   break;
+		case 4: printf("Fourty ");   break;
+		case 5: printf("Fifty ");    break;
+		case 6: printf("Sixty ");    break;
+		case 7: printf("Seventy ");  break;
+		case 8: printf("Eighty ");   break;
+		case 9: printf("Ninety ");   break;
 		}
-	Num %= 10;
-	if (Num == 11)
-		printf("Eleven ");
-	else if (Num == 12)
-		printf("Twelve ");
-	else if (Num == 13)
-		printf("Thirteen ");
-	else if (Num == 14)
-		printf("Fourteen ");
-	else if (Num == 15)
-		printf("Fifteen ");
-	else if (Num == 16)
-		printf("Sixteen ");
-	else if (Num == 17)
-		printf("Seventeen ");
-	else if (Num == 18)
-		printf("Eighteen ");
-	else if (Num == 19)
-		printf("Nineteen ");
-	else if (Num == 10)
-		printf("Ten ");
-	else if (Num < 10)
-		switch (Num)
-		{
-		case 1: printf("One "); break;
-		case 2: printf("Two "); break;
-		case 3: printf("Three "); break;
-		case 4: printf("Four "); break;
-		case 5: printf("Five "); break;
-		case 6: printf("Six "); break;
-		case 7: printf("Seven "); break;
-		case 8: printf("Eight "); break;
-		case 9: printf("Nine "); break;
+		Num%=10;
+	}
+	switch (Num)
+	{  
+		case 11: printf("Eleven ");		break;
+		case 12: printf("Twelve "); 	break;
+		case 13: printf("Thirteen ");	break;
+		case 14: printf("Fourteen ");	break;
+		case 15: printf("Fifteen ");	break;
+		case 16: printf("Sixteen ");	break;
+		case 17: printf("Seventeen ");	break;
+		case 18: printf("Eighteen ");	break;
+		case 19: printf("Nineteen ");	break;
+		case 1:  printf("One "); 		break;
+		case 2:  printf("Two "); 		break;
+		case 3:  printf("Three "); 		break;
+		case 4:  printf("Four "); 		break;
+		case 5:  printf("Five "); 		break;
+		case 6:  printf("Six "); 		break;
+		case 7:  printf("Seven "); 		break;
+		case 8:  printf("Eight "); 		break;
+		case 9:  printf("Nine "); 		break;
 		}
+}
+
+
+
+
+
+int main()
+{
+	int Num;
+	printf("\nEnter the Input : ");
+	scanf("%ld", &Num);
+	if(Num>100&&Num<1000)
+	{
+		print_digit(Num / 100);
+		printf("Hundred ");
+		if(Num%100!=0)
+			printf("and ")
+				print_digit(Num % 100);
+	}
+	else if(Num>0&&Num<100)
+		print_digit(Num);
+	else if(Num==0)
+	{
+		printf("Zero ");
+	}
+	printf("\n");
 	return 0;
 }
