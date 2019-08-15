4953ef1394b2eff9acdf5db95334beb39707032f
RazCrimson3 <bharathvigneshjo@gmail.com>
2019-08-15 23:05:59 +0530

    Update ws5_07.c

--- Programs_PC/ws5/ws5_07.c
+++ Programs_PC/ws5/ws5_07.c
@@ -3,71 +3,9 @@
 
 int main()
 {
-	int n1,n2,i;
-	short a=0;
+	int x,y;
 	printf("\nEnter the Input : ");
-	scanf("%d%d",&n1,&n2);
-<<<<<<< HEAD
-	printf("\n%d is greater than %d\n",(n1>n2)?n1:n2,(n1>n2)?n2:n1);
-	
-=======
-	printf("\n");
-	if((n1>>31)&&(((n1>>31)&1)^((n2>>31)&1)))
-	{
-		printf("%d is greater than %d\n",n2,n1);
-		return 0;
-	}
-	else if((n2>>31)&&((n1>>31)^(n2>>31)))
-	{
-		printf("%d is greater than %d\n",n1,n2);
-		return 0;
-	}
-	if((n1>>31)&&((n1>>31)^(n2>>31)))
-		printf("n1neg %d is greater than %d\n",n2,n1);
-	else if((n2>>31)&&((n1>>31)^((n2>>31))))
-		printf("%n2neg d is greater than %d\n",n1,n2);
-	else if((n2>>31)&&(n1>>31))
-	{
-		n1=~n1+1;
-		n2=~n2+1;
-		a=1;
-	}
-	for(i=30;i>=0;i--)
-	{
-		if(((n1>>i)&1)&&(((n1>>i)&1)^((n2>>i)&1)))
-		{
-			if(a)
-			{
-				n1=~n1+1;
-				n2=~n2+1;
-				a=0;
-			}
-			printf("%d is greater than %d\n",n1,n2);
-			return 0;
-			if(!(i+1))
-				break;
-		}
-		else if(((n2>>i)&1)&&(((n1>>i)&1^((n2>>i)&1))))
-		{
-			if(a)
-			{
-				n1=~n1+1;
-				n2=~n2+1;
-				a=0;
-			}
-			printf("%d is greater than %d\n",n2,n1);
-			return 0;
-			if(!(i+1))
-				break;
-		}
-	}
-	if(a)
-	{
-		n1=~n1+1;
-		n2=~n2+1;
-		a=0;
-	}
-	printf("%d is equal to %d\n",n1,n2);
+	scanf("%d%d",&x,&y);
+	printf("\nThe the greater no. is %d",x ^ ((x ^ y) & -(x < y));)
 	return 0;
->>>>>>> 03502c2e1f563d8302a795c6f27488ecb2ebdb2d
 }
