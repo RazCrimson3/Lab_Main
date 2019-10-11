cf92569eed29460abc3351e975d496bbe92dd1f9
19PW08@cs2k16.amcs.ws2k12.psgcs.edu <19PW08@cs2k16.amcs.ws2k12.psgcs.edu>
2019-10-11 10:54:38 -0700

    m

--- Programs_PC/ws7/ws7_11.c
+++ Programs_PC/ws7/ws7_11.c
@@ -4,9 +4,14 @@
 int main()
 {
     char s[] = "123456789";
-    for (int i = 0; i < 9; i++)
+    int i=0,j=0,len=strlen(s);
+    for (int i = 0; i < (len+1)/2; i++)
     {
-        printf("")
+        for(j=0;j<len-i;j++)
+            printf(" ");
+        for(j=i+1;j<2*i+1;j++)
+            print("%c ",&s[j-1]);
+        
     }
      
 }
