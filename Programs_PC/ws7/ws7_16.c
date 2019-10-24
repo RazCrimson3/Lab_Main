2d173ed79704f92cb972bc51a9e88bdad5b91769
RazCrimson3 <52282402+RazCrimson3@users.noreply.github.com>
2019-10-24 00:33:18 -0700

    Update ws7_16.c

--- Programs_PC/ws7/ws7_16.c
+++ Programs_PC/ws7/ws7_16.c
@@ -3,26 +3,25 @@
 
 int main()
 {
-        char code[][5] = {"ur","imo", "lol", "cul", "asap", "cu", "lmk", "hand"};
-        char english[][20] = {"Your","In my opinion", "Laugh out loud", "See you later", "As soon as possible", "See you", "Let me know", "Have a nice day"};
-        printf("Enter SMS abbreviation\n");
+        char shortened[][5] = {"ur","imo", "lol", "cul", "asap", "cu", "lmk", "hand"};
+        char expanded[][20] = {"Your","In my opinion", "Laugh out loud", "See you later", "As soon as possible", "See you", "Let me know", "Have a nice day"};
+        printf("Enter the SMS abbreviation:\n");
         char s1[5];
         scanf("%[^\n]s", s1);
         int i;
         int flg = 0;
         for(i=0;i<8;i++)
         {
-                if(!strcmp(code[i], s1))
+                if(!strcmp(shortened[i], s1))
                 {
                         flg = 1;
-                        printf("The equivalent English phrase is: %s\n", english[i]);
+                        printf("The equivalent English phrase is: %s\n", expanded[i]);
                 }
         }
         if(!flg)
                 printf("SMS Abbreviation not found\n");
         return 0;
 }
-
 
 
 
