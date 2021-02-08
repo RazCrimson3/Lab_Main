7fcec1cc727782ddd1d4f5d75b063d7457356505
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-02-08 09:29:59 +0530

    Minor changes

--- Sem 4/18XW48 - Python Programming Lab/Problem Sheet Basics - 2/ps_b2_10.py
+++ Sem 4/18XW48 - Python Programming Lab/Problem Sheet Basics - 2/ps_b2_10.py
@@ -1,18 +1,16 @@
-lines = input("Enter the sentence: ").split(". ")
-sentences = []
-word_set = set()
+concordance = {}
+while True:
+    line = input()
+    if line:
+        page_no = int(line[0])
+        words = line[1:].split(" ")
+        for word in words:
+            if word in concordance:
+                concordance[word].add(page_no)
+            else:
+                concordance[word] = {page_no}
+        
+    else:
+        break
 
-for line in lines:
-    words = line.split(':')[1].split()
-    sentences.append([line.split(":")[0],words])
-
-for num, words in sentences:
-    for word in words:
-        word_set.add(word)
-dictionary = {word : set() for word in word_set}
-
-for word in word_set:
-    for index, word_list in sentences:
-        if word in word_list:
-            dictionary[word].add(index)
-print(dictionary)
+print(concordance)
