350bfccf20618231fe41c6354476a041529b9915
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-02-20 20:16:38 +0530

    Networking Lab - ps3 completed

--- Sem 4/18XW46 - Computer Networks and TCP/ps3/test.py
+++ Sem 4/18XW46 - Computer Networks and TCP/ps3/test.py
@@ -5,8 +5,11 @@ host = "127.0.0.1"
 port = 8013
 
 if len(args) == 2 and args[1][0] == 's':
-    if len(args[1]) == 2 and args[1][1] == 'm':
-        from ps3_module.server_threaded import UDPServerMultiClient as Server
+    if len(args[1]) == 2:
+        if args[1][1] == 't':
+            from ps3_module.server_threaded import UDPServerMultiClient as Server
+        else:
+            from ps3_module.server_ping import UDPServer as Server
     else: 
         from ps3_module.server_loop import UDPServer as Server
 
