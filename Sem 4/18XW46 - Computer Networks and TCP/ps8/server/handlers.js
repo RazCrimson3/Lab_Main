be3cda3abcf8a68068e56452a751bf6ac53e3038
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-04-09 19:22:23 +0530

    HTTP FIle Server

---
+++ Sem 4/18XW46 - Computer Networks and TCP/ps8/server/handlers.js
@@ -0,0 +1,135 @@
+const path = require('path');
+const fs = require('fs');
+const { ENOENT } = require('constants');  // Using ENOENT as indicator for Resource Not found
+const formidable = require('formidable')
+
+
+const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+};
+
+module.exports.putHandler = (req, res, decodedURI) => {
+    res.setHeader('Content-type', 'application/json; charset=utf-8');
+    const resourcePath = path.join('./', decodedURI);
+    var responseData = '';
+
+    if (fs.existsSync(resourcePath)) {
+        fs.unlinkSync(resourcePath);
+        fs.writeFileSync(resourcePath, req.body)
+        res.statusCode = 201;
+        responseData = JSON.stringify({ code: 0, message: `Successfully modified the resource at '${decodedURI}'` });
+    }
+    else{
+        fs.writeFileSync(resourcePath, req.body)
+        res.statusCode = 201;
+        responseData = JSON.stringify({ code: 0, message: `Successfully created the resource at '${decodedURI}'` });
+    }
+    return responseData
+}
+
+module.exports.postHandler = (req, res, decodedURI) => {
+    res.setHeader('Content-type', 'application/json; charset=utf-8');
+    const resourcePath = path.join('./', decodedURI);
+    var responseData = '';
+
+    if (fs.existsSync(resourcePath)) {
+        res.statusCode = 409;
+        responseData = JSON.stringify({ code: ENOENT, message: `The resource at '${decodedURI}' already exists` });
+    }
+    else{
+        fs.writeFileSync(resourcePath, req.body)
+        res.statusCode = 201;
+        responseData = JSON.stringify({ code: 0, message: `Successfully created the resource at '${decodedURI}'` });
+    }
+    return responseData
+}
+
+
+module.exports.deleteHandler = (req, res, decodedURI) => {
+    res.setHeader('Content-type', 'application/json; charset=utf-8');
+    const resourcePath = path.join('./', decodedURI);
+    var responseData = '';
+
+    if (fs.existsSync(resourcePath)) {
+        fs.unlinkSync(resourcePath);
+        res.statusCode = 200;
+        responseData = JSON.stringify({ code: 0, message: `Successfully deleted resource at '${decodedURI}'` });
+    }
+    else{
+        res.statusCode = 404;
+        responseData = JSON.stringify({ code: ENOENT, message: `Cannot find resource at '${decodedURI}' to delete!` });
+    }
+    return responseData
+}
+
+module.exports.getHandler = (req, res, decodedURI) => {
+    const resourcePath = path.join('./', decodedURI);
+    const contentType = req.headers['x-type'] == 'json' ? 'application/json' : 'text/html';
+    var responseData = '';
+
+    if (!fs.existsSync(resourcePath)) {
+        res.statusCode = 404;
+        res.setHeader('content-type', 'application/json');
+        responseData = JSON.stringify({ code: ENOENT, message: `The resource at '${decodedURI}' cant be located!` });
+    }
+    else {
+        res.statusCode = 200;
+        if (fs.lstatSync(resourcePath).isDirectory()) {
+            const filesList = fs.readdirSync(resourcePath);
+
+            // For JSON formatted responses and HTML responses
+            var body = contentType == 'application/json' ? {} : '<ul>';
+
+            // Generating Hyperlinks to other files
+            filesList.forEach(element => {
+
+                if (fs.statSync(path.join(resourcePath, element))) {
+                    if (contentType == 'application/json')
+                        body[element] = `${decodedURI.slice(-1) != '/' ? req.url + '/' : req.url}${element}`;
+                    else
+                        body += `<br/><li> <a href='${decodedURI.slice(-1) != '/' ? req.url + '/' : req.url}${element}'> ${element} </a></li>`
+                }
+            });
+
+            if (contentType == 'application/json')
+                responseData = JSON.stringify({ code: 0, message: `List of files at '${decodedURI}'`, data: body });
+
+            else {
+                if (req.path != '/')
+                    body += `<br/><li> <a href='../'> ../ </a></li>`;
+
+                body += "</ul>";
+                responseData = `<h1>List of files at '${decodedURI}' : </h1>` + body;
+
+                res.setHeader('content-type', 'text/html');
+            }
+        }
+
+        else {
+            data = fs.readFileSync(resourcePath)
+
+            const ext = path.parse(resourcePath).ext;
+            if (mimeType[ext] != undefined)
+                res.setHeader('content-type', mimeType[ext]);
+            else
+                res.setHeader('content-type', 'text/plain');
+
+            responseData = data;
+
+        }
+    }
+    return responseData;
+}
---
+++ Sem 4/18XW46 - Computer Networks and TCP/ps8/server/server.js
@@ -0,0 +1,41 @@
+/*
+A simple file server
+
+Author: Raz Crimson
+*/
+
+const http = require('http');
+const Url = require('url-parse')
+const handlers = require("./handlers")
+
+const PORT = 3000;
+
+// Creating a server and listening at port 1800
+http.createServer((req, res) => {
+
+    var body = '';
+
+    req.on('data', chunk => body += chunk)
+
+    req.on('end', () => {
+        req.body = body;
+
+        const mode = req.method;
+        const decodedURI = decodeURI(req.url);             // To handle URI encoded characters like %20
+
+        switch (mode) {
+            case 'GET': responseData = handlers.getHandler(req, res, decodedURI); break;
+            case 'PUT': responseData = handlers.putHandler(req, res, decodedURI); break;
+            case 'POST': responseData = handlers.postHandler(req, res, decodedURI); break;
+            case 'DELETE': responseData = handlers.deleteHandler(req, res, decodedURI); break;
+            default:
+                responseData = JSON.stringify({ code: -1, message: "Invalid HTTP Method!"});
+        }
+        res.setHeader("content-length", `${responseData.length}`)
+        res.write(responseData)
+        res.end();
+    })
+
+}).listen(PORT);
+
+console.log(`Server listening on port ${PORT}`);
